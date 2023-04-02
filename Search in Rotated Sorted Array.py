03b04a2136ce26129fd1bd9bbe700213774b44e7
Bijon Mallik <bijon.ndc.pust@gmail.com>
2023-04-02 19:08:51 +0600

    Medium

---
+++ Search in Rotated Sorted Array.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 19:05:16 2023
+
+@author: Bijon
+"""
+
+class Solution:
+    def search(self, A: List[int], target: int) -> int:
+        n = len(A)
+        left, right = 0, n - 1
+        if n == 0: return -1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if A[mid] == target: return mid
+            
+            # inflection point to the right. Left is strictly increasing
+            if A[mid] >= A[left]:
+                if A[left] <= target < A[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+                    
+            # inflection point to the left of me. Right is strictly increasing
+            else:
+                if A[mid] < target <= A[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+        return -1
