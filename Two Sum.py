d9b6169ade7f599fea10c9aad16eb18889e322a3
Bijon Mallik <bijon.ndc.pust@gmail.com>
2023-04-02 21:15:48 +0600

    Easy

---
+++ Two Sum.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 21:14:27 2023
+
+@author: Bijon
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            numToIndex[nums[i]] = i
+        return []
